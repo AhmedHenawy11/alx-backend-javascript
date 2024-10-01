07f9d9417d76de3f4aa099163b15018ab8d23d0f
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-01 06:03:28 +0300

    task 11 done

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: employeesList,
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return obj;
+}
