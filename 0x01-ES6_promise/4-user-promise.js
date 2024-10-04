70405a6253f400a74ccba3b9b52cace9168fdebd
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-05 02:10:20 +0300

    task 4 fix

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,4 @@
 // Return simple promise
 export default function signUpUser(firstName, lastName) {
-  if (firstName && lastName) {
-    return Promise.resolve({firstName: firstName, lastName: lastName})
-  }
+  return Promise.resolve({ firstName, lastName });
 }
