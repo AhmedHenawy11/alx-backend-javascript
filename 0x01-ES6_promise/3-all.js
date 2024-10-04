c127ed63f6e46152de023f75cb9633f30cdd554f
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-05 01:54:39 +0300

    task 3 fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,12 @@
-/* eslint-disable */
-import { error } from 'console';
-import {uploadPhoto, createUser} from './utils.js'
+/* eslint-disable no-console */
+import { uploadPhoto, createUser } from './utils';
 
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
 
-// Promise.all : resolves when array of of promises are successfull 
-// Promise.then to access resolved object
-function handleProfileSignup() {
-    const photo = uploadPhoto();
-    const user = createUser();
-    
-    return Promise
-    .all([photo, user])
-    .then((res) => {
-        console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-    })
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
     .catch(() => { console.log('Signup system offline'); });
 }
