4689523b67209bda70d82e27eeb1e1c1aaa4549f
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-05 04:26:09 +0300

    task 6 done

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+// Promise.allSettled() : status, [value, reason]
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // checking the promises
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
+}
