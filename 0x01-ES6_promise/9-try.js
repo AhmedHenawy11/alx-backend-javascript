340653898db9d6edefee580ae754d3187ca12f6b
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-05 05:45:26 +0300

    task 9 done

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-console */
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
