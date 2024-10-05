34a269e7245c36b93fb8bbbe77153c126dd4d9f1
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-05 04:46:08 +0300

    task 8 done

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-console */
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
