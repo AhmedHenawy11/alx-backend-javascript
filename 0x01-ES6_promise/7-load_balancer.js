08203746ade12e2fd14da2adc29d6e374648d829
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-05 04:40:40 +0300

    task 7 done

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+
+
+// Promsie.race() to return faster response from the elements
+export default function loadBalancer(chinaDownload, USDownload) {
+    return Promise.race([chinaDownload, USDownload])
+      .then((value) => value);
+}
