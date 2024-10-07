161e71465591fc2cb48a3c9fe521a5253870815c
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-08 02:56:56 +0300

    task 4 done

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,10 +1,34 @@
 /* eslint-disable no-console */
-import Currency from './3-currency'
 
 export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    constructor (amount, currency) {
-        this._amount = amount;
-        this._currency = new Currency(currency);
-    }
+  // amount getter & setter
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  // currency getter & setter
+  get currencyt() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
