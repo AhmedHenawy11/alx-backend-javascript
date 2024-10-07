d6afc8ee4e4ba8e011a6c6b4ff27bfe20c2b5ee7
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-08 01:33:03 +0300

    task 3 done

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,28 @@
 /* eslint-disable no-console */
+
+export default class Currency {
+    constructor (code, name) {
+        this._code = code;
+        this._name = name;
+    }
+
+    // Code getter & setter
+    set code (value) {
+        this._code = value;
+    }
+    get code (){
+        return this._code;
+    }
+
+    // Name getter & setter
+    set code (value) {
+        this._name = value;
+    }
+    get code (){
+        return this._name;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}
