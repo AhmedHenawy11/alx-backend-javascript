d7168140956f333ac0f338add37b5e9b75587600
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-08 01:47:48 +0300

    task 3 fix

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,30 @@
 /* eslint-disable no-console */
 
 export default class Currency {
-    constructor (code, name) {
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    // Code getter & setter
-    set code (value) {
-        this._code = value;
-    }
-    get code (){
-        return this._code;
-    }
+  // Code getter & setter
+  set code(value) {
+    this._code = value;
+  }
 
-    // Name getter & setter
-    set code (value) {
-        this._name = value;
-    }
-    get code (){
-        return this._name;
-    }
+  get code() {
+    return this._code;
+  }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  // Name getter & setter
+  set code(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
