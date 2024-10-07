0536a4851a434060417713d0232fbeb1467ef9e0
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-08 01:45:16 +0300

    task 2 fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,40 @@
 /* eslint-disable no-console */
 
 export default class HolbertonCourse {
-    // Constructing object values
-    constructor(name, length, students) {
-        typeof name === 'string' ? this._name = name : (() => { throw new TypeError('Name must be a string'); })();
-        typeof length === 'number' ? this._length = length : (() => { throw new TypeError('Length must be a number'); })();
-        Array.isArray(students) ? this._students = students : (() => { throw new TypeError('Students type must be an Array') })();
-    }
+  // Constructing object values
+  constructor(name, length, students) {
+    typeof name === 'string' ? this._name = name : (() => { throw new TypeError('Name must be a string'); })();
+    typeof length === 'number' ? this._length = length : (() => { throw new TypeError('Length must be a number'); })();
+    Array.isArray(students) ? this._students = students : (() => { throw new TypeError('Students type must be an Array'); })();
+  }
 
-    // Name getter and setter
-    get name() {
-        return this._name;
-    }
+  // Name getter and setter
+  get name() {
+    return this._name;
+  }
 
-    set name(val) {
-        if (typeof val !== 'string') throw new TypeError('Name must be a string');
-        this._name = val;
-    }
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('Name must be a string');
+    this._name = val;
+  }
 
-    // Length getter and setter
-    get length() {
-        return this._length;
-    }
+  // Length getter and setter
+  get length() {
+    return this._length;
+  }
 
-    set length(val) {
-        if (!Number.isInteger(val)) throw new TypeError('Length must be a number');
-        this._length = val;
-    }
+  set length(val) {
+    if (!Number.isInteger(val)) throw new TypeError('Length must be a number');
+    this._length = val;
+  }
 
-    // Students getter and setter
-    get students() {
-        return this._students;
-    }
+  // Students getter and setter
+  get students() {
+    return this._students;
+  }
 
-    set students(val) {
-        if (!Array.isArray(val)) throw new TypeError('Students type must be an Array');
-        this._students = val;
-    }
+  set students(val) {
+    if (!Array.isArray(val)) throw new TypeError('Students type must be an Array');
+    this._students = val;
+  }
 }
-
