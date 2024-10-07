b736d87f975eaaeb7c64b4168e2a8c3b541bbc0a
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-07 09:00:07 +0300

    task 1 done

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-console */
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const object0 = new ClassRoom(19);
+  const object1 = new ClassRoom(20);
+  const object2 = new ClassRoom(34);
+
+  return [object0, object1, object2];
+}
