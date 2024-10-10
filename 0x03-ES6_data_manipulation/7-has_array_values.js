ff87654d8a6c4355df6ecfca2952937da0d4f4ac
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-10 05:09:36 +0300

    task 7 fix

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,11 @@
-export default function setFromArray(array, value) {
-  const set = new Set(array);
-  return set.has(value);
+export default function setFromArray(set, array) {
+  // Iterate through each element in the array
+  for (const value of array) {
+    // If the set doesn't have the current value, return false
+    if (!set.has(value)) {
+      return false;
+    }
+  }
+  // If all values exist in the set, return true
+  return true;
 }
