069e29c2363d64cdc387b3ff7e04094f28367985
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-10 07:41:22 +0300

    task 9 done

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const groceriesMap = new Map();
+  groceriesMap.set('Apples', 10);
+  groceriesMap.set('Tomatoes', 10);
+  groceriesMap.set('Pasta', 1);
+  groceriesMap.set('Rice', 1);
+  groceriesMap.set('Banana', 5);
+
+  return groceriesMap;
+}
